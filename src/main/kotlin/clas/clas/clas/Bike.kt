9e55f25053042726ad5bb2e4be5a48d1f25ecca0
Vika Chudnovskai <viktoria.chudnovskay@yandex.ru>
2024-04-13 13:29:37 +0300

    1. Базовое Наследование:

---
+++ src/main/kotlin/clas/clas/clas/Bike.kt
@@ -0,0 +1,7 @@
+package clas.clas.clas
+
+class Bike {
+    open val numOfWheels=""
+    open val color=""
+    open val firma=""
+}
---
+++ src/main/kotlin/clas/clas/clas/Car.kt
@@ -0,0 +1,7 @@
+package clas.clas.clas
+
+class Car {
+    open val nomer=""
+    open val color=""
+    open val firma=""
+}
